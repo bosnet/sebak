8f506c9cef48bf3f376d97d5eee9ce87a1ae5107
Geod24 <pro.mathias.lang@gmail.com>
2018-06-05 19:12:47 +0900

    Refactor amount parsing from CLI
    Reading Amount from CLI is voluntarily more relaxed than internally.
    We allow '_', ',' and '.' as separator so that users can write more readable commands.
    This was bundled in a function which has been put in utils,
    so that later commands can offer an uniform way to handle Amount.

--- cmd/sebak/common/util.go
+++ cmd/sebak/common/util.go
@@ -3,10 +3,16 @@ package common
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+
+	"github.com/owlchain/sebak/lib"
 )
 
+/**
+ * Issue a message on Stderr then exit with an error code
+ */
 func PrintFlagsError(cmd *cobra.Command, flagName string, err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: invalid '%s'; %v\n\n", flagName, err)
@@ -15,4 +21,23 @@ func PrintFlagsError(cmd *cobra.Command, flagName string, err error) {
 	cmd.Help()
 
 	os.Exit(1)
+}
+
+// Parse an input string as a monetary amount
+//
+// Commas (','), and dots ('.') and underscores ('_')
+// are treated as digit separator, and not decimal separators,
+// and will be skipped.
+//
+// Params:
+//   input = the string representation of the amount, in GON
+//
+// Returns:
+//   sebak.Amount: the value represented by `input`
+//   error: an `error`, if any happened
+func ParseAmountFromString(input string) (sebak.Amount, error) {
+	amountStr := strings.Replace(input, ",", "", -1)
+	amountStr = strings.Replace(amountStr, ".", "", -1)
+	amountStr = strings.Replace(amountStr, "_", "", -1)
+	return sebak.AmountFromString(amountStr)
 }
