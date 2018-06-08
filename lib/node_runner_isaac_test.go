b78e60557710513cae4d7372cb0488919f5f085a
Spike^ekipS <spikeekips@gmail.com>
2018-06-08 01:09:17 +0000

    #13 Transaction will be ignored when same source found in 'VotingBox' (#49)
    #13 tx will be ignored when same source found in 'VotingBox'

--- lib/node_runner_isaac_test.go
+++ lib/node_runner_isaac_test.go
@@ -114,3 +114,202 @@ func TestNodeRunnerConsensusStoreInHistoryNewBallot(t *testing.T) {
 		}
 	}
 }
+
+// TestNodeRunnerConsensusSameSourceWillBeIgnored checks, the transaction which
+// has same source will be ignored if the transaction has same source and it is
+// in 'SIGN' state.
+func TestNodeRunnerConsensusSameSourceWillBeIgnored(t *testing.T) {
+	defer sebaknetwork.CleanUpMemoryNetwork()
+
+	numberOfNodes := 3
+	nodeRunners := createNodeRunnersWithReady(numberOfNodes)
+
+	var wg sync.WaitGroup
+
+	nr0 := nodeRunners[0]
+	firstTx := makeTransaction(nr0.Node().Keypair())
+	secondTx := makeTransaction(nr0.Node().Keypair())
+
+	var handleBallotCheckerFuncs = []sebakcommon.CheckerFunc{
+		CheckNodeRunnerHandleBallotIsWellformed,
+		CheckNodeRunnerHandleBallotCheckIsNew,
+		CheckNodeRunnerHandleBallotReceiveBallot,
+
+		// stop consensus when reached 'SIGN'
+		func(c sebakcommon.Checker, args ...interface{}) (err error) {
+			checker := c.(*NodeRunnerHandleBallotChecker)
+
+			if checker.Ballot.MessageHash() != firstTx.GetHash() {
+				return
+			}
+
+			if checker.VotingStateStaging.State == sebakcommon.BallotStateSIGN {
+				err = sebakcommon.CheckerErrorStop{"stop consensus, because it is in SIGN state"}
+				wg.Done()
+				return
+			}
+
+			return
+		},
+		CheckNodeRunnerHandleBallotHistory,
+		CheckNodeRunnerHandleBallotStore,
+		CheckNodeRunnerHandleBallotIsBroadcastable,
+		CheckNodeRunnerHandleBallotVotingHole,
+		CheckNodeRunnerHandleBallotBroadcast,
+	}
+
+	for _, nr := range nodeRunners {
+		nr.SetHandleBallotCheckerFuncs(nil, handleBallotCheckerFuncs...)
+	}
+
+	client := nr0.Network().GetClient(nr0.Node().Endpoint())
+
+	wg.Add(3)
+	client.SendMessage(firstTx)
+	wg.Wait()
+
+	isaac := nr0.Consensus().(*ISAAC)
+	if !isaac.HasMessage(firstTx) {
+		t.Error("transaction not found")
+		return
+	}
+
+	if _, ok := isaac.Boxes.Results[firstTx.GetHash()]; !ok {
+		t.Error("VotingResult not found")
+		return
+	}
+
+	if !isaac.Boxes.VotingBox.HasMessage(firstTx) {
+		t.Error("ballot not in VotingBox")
+		return
+	}
+
+	var deferFunc sebakcommon.CheckerDeferFunc = func(n int, c sebakcommon.Checker, err error) {
+		if err == nil {
+			return
+		}
+
+		if _, ok := err.(sebakcommon.CheckerErrorStop); ok {
+			wg.Done()
+			return
+		}
+	}
+
+	for _, nr := range nodeRunners {
+		nr.SetHandleMessageFromClientCheckerFuncs(deferFunc)
+	}
+
+	wg = sync.WaitGroup{}
+	wg.Add(1)
+	client.SendMessage(secondTx)
+	wg.Wait()
+
+	if isaac.HasMessage(secondTx) {
+		t.Error("second transaction was added as VotingResult")
+		return
+	}
+}
+
+// TestNodeRunnerConsensusSameSourceWillNotIgnored checks, the transaction which
+// has same source will be ignored if the transaction has same source and it is
+// not in 'SIGN' state.
+func TestNodeRunnerConsensusSameSourceWillNotIgnored(t *testing.T) {
+	defer sebaknetwork.CleanUpMemoryNetwork()
+
+	numberOfNodes := 3
+	nodeRunners := createNodeRunnersWithReady(numberOfNodes)
+
+	var wg sync.WaitGroup
+
+	nr0 := nodeRunners[0]
+	firstTx := makeTransaction(nr0.Node().Keypair())
+	secondTx := makeTransaction(nr0.Node().Keypair())
+
+	var handleBallotCheckerFuncs = []sebakcommon.CheckerFunc{
+		CheckNodeRunnerHandleBallotIsWellformed,
+		CheckNodeRunnerHandleBallotCheckIsNew,
+		CheckNodeRunnerHandleBallotReceiveBallot,
+
+		// stop consensus when reached 'INIT'
+		func(c sebakcommon.Checker, args ...interface{}) (err error) {
+			err = sebakcommon.CheckerErrorStop{"stop consensus, because it is in INIT state"}
+			defer wg.Done()
+			return
+		},
+		CheckNodeRunnerHandleBallotHistory,
+		CheckNodeRunnerHandleBallotStore,
+		CheckNodeRunnerHandleBallotIsBroadcastable,
+		CheckNodeRunnerHandleBallotVotingHole,
+		CheckNodeRunnerHandleBallotBroadcast,
+	}
+
+	for _, nr := range nodeRunners {
+		nr.SetHandleBallotCheckerFuncs(nil, handleBallotCheckerFuncs...)
+	}
+
+	client := nr0.Network().GetClient(nr0.Node().Endpoint())
+
+	wg.Add(2)
+	client.SendMessage(firstTx)
+	wg.Wait()
+
+	isaac := nr0.Consensus().(*ISAAC)
+	if !isaac.HasMessage(firstTx) {
+		t.Error("transaction not found")
+		return
+	}
+
+	if _, ok := isaac.Boxes.Results[firstTx.GetHash()]; !ok {
+		t.Error("VotingResult not found")
+		return
+	}
+
+	if !isaac.Boxes.WaitingBox.HasMessage(firstTx) {
+		t.Error("ballot not in WaitingBox")
+		return
+	}
+
+	var lastVotingStateStaging []VotingStateStaging
+	var deferFunc sebakcommon.CheckerDeferFunc = func(n int, c sebakcommon.Checker, err error) {
+		if err == nil {
+			return
+		}
+
+		if _, ok := err.(sebakcommon.CheckerErrorStop); !ok {
+			return
+		}
+
+		checker := c.(*NodeRunnerHandleBallotChecker)
+		if checker.VotingStateStaging.IsEmpty() {
+			return
+		}
+
+		if !checker.VotingStateStaging.IsClosed() {
+			return
+		}
+
+		lastVotingStateStaging = append(lastVotingStateStaging, checker.VotingStateStaging)
+		wg.Done()
+	}
+
+	for _, nr := range nodeRunners {
+		nr.SetHandleBallotCheckerFuncs(deferFunc, DefaultHandleBallotCheckerFuncs...)
+	}
+
+	wg = sync.WaitGroup{}
+	wg.Add(3)
+	client.SendMessage(secondTx)
+	wg.Wait()
+
+	if len(lastVotingStateStaging) != 3 {
+		t.Error("failed to get consensus")
+		return
+	}
+
+	for _, vs := range lastVotingStateStaging {
+		if !vs.IsClosed() {
+			t.Error("failed to close consensus")
+			return
+		}
+	}
+}
