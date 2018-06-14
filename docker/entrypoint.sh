e978ad7f385f873c8f17a5da5650f82b00e0839a
Geod24 <pro.mathias.lang@gmail.com>
2018-06-14 18:29:25 +0900

    Allow to pass arbitrary arguments to sebak
    This will allow us to test the command line interface trivially
    while keeping the node behavior working.
    It currently does not support running sebak multiple times
    (as done in the node mode, by called genesis first),
    but this can be fixed if/when needed.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-## Allow us to split the $SEBAK_VALIDATORS 'array' into multiple `--validator` args
-## Note: It's not a proper array since the shell we use is `sh`, not `bash`
-./sebak genesis ${SEBAK_GENESIS_BLOCK}
-for VALIDATOR in ${SEBAK_VALIDATORS}; do
-    VALIDATOR_ARGS="${VALIDATOR_ARGS} --validator=${VALIDATOR}"
-done
-./sebak node ${VALIDATOR_ARGS} --network-id=${SEBAK_NETWORK_ID} --secret-seed=${SEBAK_SECRET_SEED} --log-level debug
+## This entrypoint has 2 modes: if any argument is provided to `docker run`,
+## the arguments are passed directly to sebak
+## Otherwise, it just starts a node with the environment file
+if [ $# -gt 1 ]; then
+    ## Argument mode
+    ./sebak $@
+else
+    ## Node mode
+
+    ## Allow us to split the $SEBAK_VALIDATORS 'array' into multiple `--validator` args
+    ## Note: It's not a proper array since the shell we use is `sh`, not `bash`
+    ./sebak genesis ${SEBAK_GENESIS_BLOCK}
+    for VALIDATOR in ${SEBAK_VALIDATORS}; do
+        VALIDATOR_ARGS="${VALIDATOR_ARGS} --validator=${VALIDATOR}"
+    done
+    ./sebak node ${VALIDATOR_ARGS} --network-id=${SEBAK_NETWORK_ID} --secret-seed=${SEBAK_SECRET_SEED} --log-level debug
+fi
