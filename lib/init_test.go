a797276d13b4cd1189213699f8b59d8a3dcd190f
JeongHoon Byun (aka Outsider) <outsideris@gmail.com>
2018-06-21 18:45:12 +0900

    Logs in tests are enabled/disabled now (#117)

--- lib/init_test.go
+++ lib/init_test.go
@@ -1,13 +1,13 @@
 package sebak
 
 import (
-	"os"
-
 	logging "github.com/inconshreveable/log15"
+
+	"boscoin.io/sebak/lib/common/test"
 )
 
 var networkID []byte = []byte("sebak-test-network")
 
 func init() {
-	SetLogging(logging.LvlDebug, logging.StreamHandler(os.Stdout, logging.TerminalFormat()))
+	SetLogging(logging.LvlDebug, test.LogHandler())
 }
