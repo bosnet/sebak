4d9778f4fa52955050f05cae90fbcc8302076daa
Spike^ekipS <spikeekips@gmail.com>
2018-06-27 16:34:05 +0900

    multiple validators can be set by `SEBAK_VALIDATORS` env variable

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,8 +12,5 @@ else
     ## Allow us to split the $SEBAK_VALIDATORS 'array' into multiple `--validator` args
     ## Note: It's not a proper array since the shell we use is `sh`, not `bash`
     ./sebak genesis ${SEBAK_GENESIS_BLOCK}
-    for VALIDATOR in ${SEBAK_VALIDATORS}; do
-        VALIDATOR_ARGS="${VALIDATOR_ARGS} --validator=${VALIDATOR}"
-    done
-    ./sebak node ${VALIDATOR_ARGS} --network-id=${SEBAK_NETWORK_ID} --secret-seed=${SEBAK_SECRET_SEED} --log-level debug
+    ./sebak node --log-level debug
 fi
