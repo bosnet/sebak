00f8677f4002389b1c2ffbe5d402919a81c886b5
Spike^ekipS <spikeekips@gmail.com>
2018-06-27 06:25:13 +0100

    #59 `BallotBoxes` has `Sources` to filter the transactions from same source (#119)
    * #59 BallotBoxes has `Sources` to handle the ballot sources

--- lib/isaac.go
+++ lib/isaac.go
@@ -130,6 +130,7 @@ func (is *ISAAC) receiveBallotStateINIT(ballot Ballot) (vs VotingStateStaging, e
 		is.Boxes.WaitingBox.RemoveVotingResult(vr) // TODO detect error
 		if !vs.IsClosed() {
 			is.Boxes.VotingBox.AddVotingResult(vr) // TODO detect error
+			is.Boxes.AddSource(ballot)
 		}
 	}
 
@@ -167,6 +168,10 @@ func (is *ISAAC) CloseConsensus(ballot Ballot) (err error) {
 func (is *ISAAC) receiveBallotVotingStates(ballot Ballot) (vs VotingStateStaging, err error) {
 	if _, err = is.Boxes.AddBallot(ballot); err != nil {
 		return
+	}
+
+	if !is.Boxes.VotingBox.HasMessageByHash(ballot.MessageHash()) {
+		is.Boxes.AddSource(ballot)
 	}
 
 	vr := is.Boxes.VotingResult(ballot)
