d0291d8f7c32bbdd506cb0e982e37a59a906faef
Spike^ekipS <spikeekips@gmail.com>
2018-06-29 07:01:26 +0900

    remove unused comments

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-## This entrypoint has 2 modes: if any argument is provided to `docker run`,
-## the arguments are passed directly to sebak
-## Otherwise, it just starts a node with the environment file
+# This entrypoint has 2 modes: if any argument is provided to `docker run`, the
+# arguments are passed directly to sebak Otherwise, it just starts a node with the
+# environment file
 if [ $# -gt 1 ]; then
-    ## Argument mode
+    # Argument mode
     ./sebak $@
 else
-    ## Node mode
-
-    ## Allow us to split the $SEBAK_VALIDATORS 'array' into multiple `--validator` args
-    ## Note: It's not a proper array since the shell we use is `sh`, not `bash`
+    # Node mode
     ./sebak genesis ${SEBAK_GENESIS_BLOCK}
     ./sebak node --log-level debug
 fi
