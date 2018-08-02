967ed1d38a1638fead011e64f187fb287d9e1de9
Spike^ekipS <spikeekips@gmail.com>
2018-08-02 12:56:29 +0900

    merged with siyong work

--- lib/block_header.go
+++ lib/block_header.go
@@ -5,13 +5,13 @@ import (
 )
 
 type Header struct {
-	Version                 uint32
-	PrevBlockHash           string // [TODO] Uint256 type
-	TransactionsRoot        string // Merkle root of Txs [TODO] Uint256 type
-	Timestamp               time.Time
-	Height                  uint64
-	TotalTxs                uint64
-	prevConsensusResultHash string // [TODO] Uint256 type
+	Version          uint32
+	PrevBlockHash    string // [TODO] Uint256 type
+	TransactionsRoot string // Merkle root of Txs [TODO] Uint256 type
+	Timestamp        time.Time
+	Height           uint64
+	TotalTxs         uint64
+	// prevConsensusResultHash string // [TODO] Uint256 type
 	// ConsensusPayloadHash    Uint256
 	// ConsensusPayload        Payload  // or []byte
 	// StateRoot types.Hash    // MPT of state
@@ -19,21 +19,22 @@ type Header struct {
 }
 
 func NewBlockHeader(height uint64, prevBlockHash string, prevTotalTxs uint64, currentTxs uint64, txRoot string) *Header {
-	p := Header{
+	return &Header{
 		PrevBlockHash:    prevBlockHash,
 		Timestamp:        time.Now(),
 		Height:           height,
 		TotalTxs:         prevTotalTxs + currentTxs,
 		TransactionsRoot: txRoot,
 	}
-	p.fill()
-	return &p
-}
-func (h *Header) fill() {
-	// [TODO] fill
+	//p.fill()
+	//return &p
 }
 
-type ConsensusResult struct {
-	BlockHash string // [TODO] Uint256 type
-	Ballots   []*Ballot
-}
+// func (h *Header) fill() {
+// 	// [TODO] fill
+// }
+
+// type ConsensusResult struct {
+// 	BlockHash string // [TODO] Uint256 type
+// 	Ballots   []*Ballot
+// }
