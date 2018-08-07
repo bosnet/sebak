75b0c369231b8e843b2ccb6b25d3f19fe5b6282e
Marshall Kim <marshall.kim@buzzvil.com>
2018-08-07 17:27:05 +0900

    Contract (#219)
    * contract package
    
    * remove useless code
    
    * fmt
    
    * dummy wasm

---
+++ lib/contract/jsvm/deployer.go
@@ -0,0 +1,42 @@
+package jsvm
+
+import (
+	"boscoin.io/sebak/lib/contract/context"
+	"boscoin.io/sebak/lib/contract/payload"
+	"github.com/robertkrimen/otto"
+)
+
+type JsDeployer struct {
+	Context *context.Context
+}
+
+func NewDeployer(context *context.Context) *JsDeployer {
+
+	de := &JsDeployer{
+		Context: context,
+	}
+	return de
+}
+
+func (jd *JsDeployer) Deploy(codeByte []byte) (err error) {
+
+	var deployCode = new(payload.DeployCode)
+	deployCode.ContractAddress = jd.Context.SenderAccount.Address
+	deployCode.Code = codeByte
+	deployCode.Type = payload.JavaScript
+
+	if err = jd.compile(deployCode.Code); err != nil {
+		return
+	}
+
+	jd.Context.StateClone.PutDeployCode(deployCode)
+	return nil
+}
+
+func (jd *JsDeployer) compile(code []byte) (err error) {
+	vm := otto.New()
+
+	_, err = vm.Compile("", code)
+
+	return
+}
