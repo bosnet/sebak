4e96996b25c2bd6d03f5c95b78b5e0d5fa6535d3
kfangw <kfangw@gmail.com>
2018-08-07 17:24:53 +0900

    dummy wasm

--- lib/contract/wasm/executor.go
+++ lib/contract/wasm/executor.go
@@ -4,15 +4,17 @@ import (
 	"boscoin.io/sebak/lib/contract/context"
 	"boscoin.io/sebak/lib/contract/payload"
 	"boscoin.io/sebak/lib/contract/value"
-	"encoding/binary"
-	"github.com/ontio/ontology-wasm/exec"
+	"github.com/go-interpreter/wagon/exec"
+	"os"
+	"github.com/go-interpreter/wagon/wasm"
+	"github.com/go-interpreter/wagon/validate"
 )
 
 type ExecFunc func(e *WasmExecutor, code *payload.ExecCode) (*value.Value, error)
 
 type WasmExecutor struct {
 	Context *context.Context
-	Engine  *exec.ExecutionEngine
+	//Engine  *exec.ExecutionEngine
 }
 
 func NewWasmExecutor(ctx *context.Context) *WasmExecutor {
@@ -23,30 +25,47 @@ func NewWasmExecutor(ctx *context.Context) *WasmExecutor {
 	return ex
 }
 
+//TODO: it is just a dummy
 func (ex *WasmExecutor) Execute(c *payload.ExecCode) (retCode *value.Value, err error) {
-
-	engine := exec.NewExecutionEngine(nil, "product")
-
-	//TODO: set wasmcode and input
-	var wasmcode []byte //wasm byte code
-	var input []byte    //method | arguments...
-	var result value.Value
-
-	{
-		//TODO: encode input to byte
-	}
-	res, err := engine.Call(nil, wasmcode, input)
+	file, err := os.Open("dummy")
 	if err != nil {
-		return nil, err
+		return
+	}
+	defer file.Close()
+
+	module, err := wasm.ReadModule(file, nil)
+	if err != nil {
+		return
+	}
+	if err = validate.VerifyModule(module); err != nil {
+		return
 	}
 
-	{
-		//TODO: decode return byte to ReturnCode
-		_, err = engine.GetVM().GetPointerMemory(uint64(binary.LittleEndian.Uint32(res)))
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return &result, err
+	exec.NewVM(module)
+	//
+	//engine := exec.NewExecutionEngine(nil, "product")
+	//
+	////TODO: set wasmcode and input
+	//var wasmcode []byte //wasm byte code
+	//var input []byte    //method | arguments...
+	//var result value.Value
+	//
+	//{
+	//	//TODO: encode input to byte
+	//}
+	//res, err := engine.Call(nil, wasmcode, input)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//{
+	//	//TODO: decode return byte to ReturnCode
+	//	_, err = engine.GetVM().GetPointerMemory(uint64(binary.LittleEndian.Uint32(res)))
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//}
+	//
+	//return &result, err
+	return
 }
