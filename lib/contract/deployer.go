833f8e8dac37ca256db8ee072c6e497ba1726370
Marshall Kim <marshall.kim@buzzvil.com>
2018-08-08 17:19:18 +0900

    contract deploy/execute operation (#228)
    * contract deploy/execute operation without testcode
    
    * remove the do method

--- lib/contract/deployer.go
+++ lib/contract/deployer.go
@@ -3,18 +3,19 @@ package contract
 import (
 	"boscoin.io/sebak/lib/contract/context"
 	"boscoin.io/sebak/lib/contract/jsvm"
+	"boscoin.io/sebak/lib/contract/payload"
 )
 
 type Deployer interface {
 	Deploy(code []byte) error
 }
 
-func NewDeployer(ctx *context.Context, codeType string) Deployer {
+func NewDeployer(ctx *context.Context, codeType payload.CodeType) Deployer {
 
 	var de Deployer
 
 	switch codeType {
-	case "otto":
+	case payload.JavaScript:
 		de = jsvm.NewDeployer(ctx)
 	default:
 		panic("not yet supported")
@@ -23,7 +24,7 @@ func NewDeployer(ctx *context.Context, codeType string) Deployer {
 
 }
 
-func Deploy(ctx *context.Context, codeType string, code []byte) (err error) {
+func Deploy(ctx *context.Context, codeType payload.CodeType, code []byte) (err error) {
 	deployer := NewDeployer(ctx, codeType)
 	err = deployer.Deploy(code)
 	return
