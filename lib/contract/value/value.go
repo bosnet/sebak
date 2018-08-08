d620a9aec693144aa8feb56e0a105cf6dcf3521c
kfangw <kfangw@gmail.com>
2018-08-08 17:15:11 +0900

    value encode/decode
    go fmt
    
    refactoring

--- lib/contract/value/value.go
+++ lib/contract/value/value.go
@@ -2,60 +2,131 @@ package value
 
 import (
 	"encoding/binary"
+	"errors"
 	"github.com/robertkrimen/otto"
-	"io"
 )
 
-type Type int
+type Type byte
 
 const (
-	Nil Type = iota
-	Int
-	String
-	Array
-	Bytes
+	Nil Type = 0x00
+	SInt     = 0x01
+	UInt     = 0x02
+	String   = 0x03
+	Boolean  = 0x04
+)
+
+const (
+	True  = 0x01
+	False = 0x00
 )
 
 type Value struct {
-	Type     Type
-	Contents []byte
+	Type    Type
+	value   interface{}
 }
 
-func (v *Value) Serialize(w io.Writer) error {
+func ToValue(iv interface{}) (v *Value, err error) {
+	v = &Value{}
 
-	return nil
-}
-
-func (v *Value) Deserialize(r io.Reader) error {
-
-	return nil
-}
-
-func StringValue(s string) *Value {
-	v := &Value{
-		Type:     String,
-		Contents: []byte(s),
+	switch iv.(type) {
+	case otto.Value:
+		iv, err = iv.(otto.Value).Export()
+	case []byte:
+		encoded := iv.([]byte)[1:]
+		switch Type(iv.([]byte)[0]) {
+		case Nil:
+			iv = nil
+		case String:
+			iv = string(encoded)
+		case SInt:
+			iv = int64(binary.LittleEndian.Uint64(encoded))
+		case UInt:
+			iv = binary.LittleEndian.Uint64(encoded)
+		case Boolean:
+			if encoded[0] == True {
+				iv = true
+			} else {
+				iv = false
+			}
+		default:
+			iv = nil
+		}
+	default:
+		// iv = iv
 	}
-	return v
-}
 
-func ToValue(ottoValue otto.Value) (*Value, error) {
-	//ottoValue.Export()
-	var v = new(Value)
-	var err error
-	if ottoValue.IsNumber() {
-		var intValue int64
-		intValue, err = ottoValue.ToInteger()
-		v.Type = Int
-		v.Contents = make([]byte, 4)
-		binary.LittleEndian.PutUint64(v.Contents, uint64(intValue))
-	} else if ottoValue.IsString() {
-		var strValue string
-		strValue, err = ottoValue.ToString()
+	v.value = iv
+
+	switch iv.(type) {
+	case nil:
+		v.Type = Nil
+	case string:
 		v.Type = String
-		v.Contents = []byte(strValue)
-	} else {
-		panic("not yet supported type")
+	case bool:
+		v.Type = Boolean
+	case int:
+		v.Type = SInt
+		v.value = int64(v.value.(int))
+	case int8:
+		v.Type = SInt
+		v.value = int64(v.value.(int8))
+	case int16:
+		v.Type = SInt
+		v.value = int64(v.value.(int16))
+	case int32:
+		v.Type = SInt
+		v.value = int64(v.value.(int32))
+	case int64:
+		v.Type = SInt
+		v.value = int64(v.value.(int64))
+	case uint:
+		v.Type = UInt
+		v.value = uint64(v.value.(uint))
+	case uint8:
+		v.Type = UInt
+		v.value = uint64(v.value.(uint8))
+	case uint16:
+		v.Type = UInt
+		v.value = uint64(v.value.(uint16))
+	case uint32:
+		v.Type = UInt
+		v.value = uint64(v.value.(uint32))
+	case uint64:
+		v.Type = UInt
+		v.value = uint64(v.value.(uint64))
+	case float32, float64:
+		v.Type = Nil
+		err = errors.New("not yet supported type")
+	default:
+		v.Type = Nil
+		err = errors.New("not yet supported type")
 	}
-	return v, err
+	return
+}
+
+func (v *Value) Serialize() (encoded []byte, err error) {
+
+	switch v.Type {
+	case Nil:
+		encoded = []byte{}
+	case SInt:
+		encoded = make([]byte, 8)
+		binary.LittleEndian.PutUint64(encoded, uint64(v.value.(int64)))
+	case UInt:
+		encoded = make([]byte, 8)
+		binary.LittleEndian.PutUint64(encoded, uint64(v.value.(uint64)))
+	case String:
+		encoded = []byte(v.value.(string))
+	case Boolean:
+		if v.value.(bool) {
+			encoded = []byte{True}
+		} else {
+			encoded = []byte{False}
+		}
+	}
+
+	encoded = append([]byte{byte(v.Type)}, encoded...)
+
+	return
 }
