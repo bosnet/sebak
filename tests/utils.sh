36be761f2587679d8dde98377f396ce8fb3e1dce
Spike^ekipS <spikeekips@gmail.com>
2018-08-14 15:15:13 +0900

    fixed the failure of integration test

--- tests/utils.sh
+++ tests/utils.sh
@@ -19,3 +19,28 @@ function getAccount () # u16 port, string addr
                   https://127.0.0.1:${1}/api/account/${2} \
                   2>/dev/null)
 }
+
+function getAccountWithBalance () # u16 port, string addr and expected balance
+{
+    if [ $# -ne 3 ]; then
+        die "3 arguments expected for getAccountWithBalance, not $#"
+    fi
+
+    balance=0
+    for i in $(seq 30)
+    do
+	    balance=$(curl --insecure \
+                  --request GET \
+                  --header "Accept: application/json" \
+                  https://127.0.0.1:${1}/api/account/${2} \
+                  2>/dev/null | jq ".Balance" | sed 's/"//g')
+	    if [ "$balance" == "$3" ];then
+		    return 0
+	    fi
+	    sleep 0.3
+    done
+
+    echo $balance
+
+    return 1
+}
