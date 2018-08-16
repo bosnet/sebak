2addfe498ce695af8e33520d87481bd8dc76aaae
Spike^ekipS <spikeekips@gmail.com>
2018-08-16 16:45:39 +0900

    Integration Tests: Use retry and sleep for balance check (#253)
    * fixed the failure of integration test

--- tests/utils.sh
+++ tests/utils.sh
@@ -19,3 +19,24 @@ function getAccount () # u16 port, string addr
                   https://127.0.0.1:${1}/api/account/${2} \
                   2>/dev/null)
 }
+
+function getAccountWithBalance () # u16 port, string addr and expected balance
+{
+    if [ $# -ne 3 ]; then
+        die "3 arguments expected for getAccountWithBalance, not $#"
+    fi
+
+    balance=0
+    for i in $(seq 30)
+    do
+	    balance=$(getAccount $1 $2 | jq ".Balance" | sed 's/"//g')
+	    if [ "$balance" == "$3" ];then
+		    return 0
+	    fi
+	    sleep 0.3
+    done
+
+    echo $balance
+
+    return 1
+}
