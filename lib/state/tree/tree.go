8289b82c1d1f24fc542c5aa9dbdaaeddbf9d56b9
anarcher <anarcher@gmail.com>
2018-08-28 16:44:36 +0900

    Change builder to loader in tree. and Tree separates MutableTree and ImmutableTree

--- lib/state/tree/tree.go
+++ lib/state/tree/tree.go
@@ -3,18 +3,27 @@ package tree
 // Tree is the interface that is Merkle tree db
 //
 // eth's trie or tendermint's IAVL can be an implementation of this interface
-type Tree interface {
-	Set(key, value []byte) error
+type ImmutableTree interface {
 	Get(key []byte) (value []byte, err error)
+	Hash() []byte //current working root hash
+}
+
+type MutableTree interface {
+	ImmutableTree
+
+	Set(key, value []byte) error
 	Delete(key []byte) error
 
-	Hash() []byte //current working root hash
 	Commit() ([]byte, error)
 }
 
-// Builder is the interface that make new tree
+type Tree = MutableTree
+
+// Loader is the interface that make new tree
 //
-// Build returns new tree db related with the hash
-type Builder interface {
-	Build(hash []byte) (Tree, error)
+// LoadMutableTree returns new tree db related with the hash
+// LoadImmutableTree too.
+type Loader interface {
+	LoadMutableTree(hash []byte) (MutableTree, error)
+	LoadImmutableTree(hash []byte) (ImmutableTree, error)
 }
