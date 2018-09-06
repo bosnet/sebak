7984d75fe52f2a91ab04497f0dbf884756d71a63
anarcher <anarcher@gmail.com>
2018-09-06 17:33:39 +0900

    Add comments for IsEventStream

--- lib/httputils/httputils.go
+++ lib/httputils/httputils.go
@@ -2,6 +2,7 @@ package httputils
 
 import "net/http"
 
+// IsEventStream checks request header accept is text/event-stream
 func IsEventStream(r *http.Request) bool {
 	if r.Header.Get("Accept") == "text/event-stream" {
 		return true
