ede00e74db315fbcd6a796de7240fb945ac7609f
anarcher <anarcher@gmail.com>
2018-09-11 11:27:12 +0900

    WriteJSON support hal.Resource

--- lib/network/httputils/json.go
+++ lib/network/httputils/json.go
@@ -3,12 +3,24 @@ package httputils
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/nvellon/hal"
 )
+
+type HALResource interface {
+	Resource() *hal.Resource
+}
 
 // WriteJSON writes the value v to the http response as json encoding
 func WriteJSON(w http.ResponseWriter, code int, v interface{}) error {
-	w.Header().Set("content-type", "application/json")
 	w.WriteHeader(code)
+
+	if h, ok := v.(HALResource); ok {
+		w.Header().Set("content-type", "application/hal+json")
+		v = h.Resource()
+	} else {
+		w.Header().Set("content-type", "application/json")
+	}
 
 	bs, err := json.Marshal(v)
 	if err != nil {
