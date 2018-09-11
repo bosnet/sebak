9ac3cfcfe618bc9f83dc7a07aedfe31a8dff4b2c
kfangw <kfangw@gmail.com>
2018-09-11 10:50:46 +0900

    rebase

--- lib/node/noderunner/isaac_simulation_test.go
+++ lib/node/noderunner/isaac_simulation_test.go
@@ -12,6 +12,7 @@ import (
 
 	"boscoin.io/sebak/lib/common"
 	"boscoin.io/sebak/lib/consensus/round"
+	"boscoin.io/sebak/lib/transaction"
 )
 
 /*
@@ -25,7 +26,7 @@ TestISAACSimulationProposer indicates the following:
 func TestISAACSimulationProposer(t *testing.T) {
 	nodeRunners := createTestNodeRunner(5)
 
-	tx, txByte := getTransaction(t)
+	tx, txByte := transaction.GetTransaction(t)
 
 	message := common.NetworkMessage{Type: common.TransactionMessage, Data: txByte}
 
