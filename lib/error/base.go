ff1ce06f1f5a59178420ce11f704b2339c239ba7
Spike^ekipS <spikeekips@gmail.com>
2018-09-25 17:36:23 +0900

    errors.Error had problem to rlp.Encode()

--- lib/error/base.go
+++ lib/error/base.go
@@ -1,11 +1,17 @@
 package errors
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"io"
+	"sort"
+
+	"github.com/ethereum/go-ethereum/rlp"
+)
 
 type Error struct {
-	Code    uint   `json:"code"`
-	Message string `json:"message"`
-	Data    map[string]interface{}
+	Code    uint                   `json:"code"`
+	Message string                 `json:"message"`
+	Data    map[string]interface{} `json:"data" rlp:"-"`
 }
 
 func (o *Error) Serialize() (b []byte, err error) {
@@ -18,7 +24,6 @@ func (o *Error) Error() string {
 	return string(b)
 }
 
-// SetData sets `Error.Data`
 func (o *Error) SetData(k string, v interface{}) *Error {
 	o.Data[k] = v
 
@@ -26,18 +31,45 @@ func (o *Error) SetData(k string, v interface{}) *Error {
 }
 
 func (o *Error) Clone() *Error {
-	data := map[string]interface{}{}
-	if o.Data != nil {
+	var new Error
+	new = *o
+
+	new.Data = map[string]interface{}{}
+	if o.Data != nil && len(o.Data) > 0 {
 		for k, v := range o.Data {
-			data[k] = v
+			new.Data[k] = v
 		}
 	}
 
-	return &Error{
+	return &new
+}
+
+func (o *Error) EncodeRLP(w io.Writer) (err error) {
+	if o == nil {
+		return rlp.Encode(w, []uint{})
+	}
+
+	if o.Data != nil && len(o.Data) > 0 {
+		var d [][2]interface{}
+
+		var keys []string
+		for k, _ := range o.Data {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		for _, k := range keys {
+			d = append(d, [2]interface{}{k, o.Data[k]})
+		}
+		err = rlp.Encode(w, d)
+	}
+
+	return rlp.Encode(w, struct {
+		Code    uint
+		Message string
+	}{
 		Code:    o.Code,
 		Message: o.Message,
-		Data:    data,
-	}
+	})
 }
 
 func NewError(code uint, message string) *Error {
