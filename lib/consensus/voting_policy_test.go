2855f55444cf715578b8d120d8960e750106b1df
Charles <charleslee522@gmail.com>
2018-10-01 21:37:57 +0900

    Add checking threshold unit test

---
+++ lib/consensus/voting_policy_test.go
@@ -0,0 +1,16 @@
+package consensus
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestThreshold(t *testing.T) {
+	vt, err := NewDefaultVotingThresholdPolicy(66)
+	require.Nil(t, err)
+
+	vt.SetValidators(1000)
+	require.Equal(t, 660, vt.Threshold())
+
+}
