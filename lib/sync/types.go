a7c27e97fb908ac7ccda1827ffd59196709e0d10
anarcher <anarcher@gmail.com>
2018-10-02 14:05:30 +0900

    Add interface of sync controller

--- lib/sync/types.go
+++ lib/sync/types.go
@@ -9,6 +9,16 @@ import (
 	"boscoin.io/sebak/lib/transaction"
 )
 
+type SyncProgress struct {
+	StartingBlock uint64 // Block number where sync began
+	CurrentBlock  uint64 // Current block number where sync is at
+	HighestBlock  uint64 // Highest alleged block number in the chain
+}
+
+type SyncController interface {
+	SetSyncTargetBlock(ctx context.Context, height uint64) error
+}
+
 type SyncInfo struct {
 	BlockHeight uint64
 	Block       *block.Block
