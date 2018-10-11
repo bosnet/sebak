ae4bd1593166d7c0953fb2f9ab7ccf09564db366
anarcher <anarcher@gmail.com>
2018-10-11 15:15:51 +0900

    Change http_middlewares_test using httptest.Server

--- lib/network/http_middlewares_test.go
+++ lib/network/http_middlewares_test.go
@@ -2,56 +2,41 @@ package network
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
-	"boscoin.io/sebak/lib/common"
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/require"
 )
 
 func TestRecoverMiddleware(t *testing.T) {
-	endpoint, err := common.NewEndpointFromString(
-		fmt.Sprintf("http://localhost:%s", getPort()),
-	)
-	require.Nil(t, err)
-
-	network, err := makeTestHTTP2NetworkForTLS(endpoint)
-	require.Nil(t, err)
-	defer network.Stop()
-
 	handlerURL := UrlPathPrefixAPI + "/test"
 	panicMsg := "Don't panic,just use go"
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		panic(panicMsg)
 	}
 
-	VerboseLogs = false
-	network.AddMiddleware(RouterNameAPI, RecoverMiddleware(nil))
-	network.AddHandler(handlerURL, handler)
+	router := mux.NewRouter()
+	router.Use(RecoverMiddleware(nil))
+	router.HandleFunc(handlerURL, http.HandlerFunc(handler)).Methods("GET")
 
-	{
-		// with normal HTTP2Client
-		client, err := common.NewHTTP2Client(
-			defaultTimeout,
-			defaultIdleTimeout,
-			false,
-		)
-		require.Nil(t, err)
+	ts := httptest.NewServer(router)
+	defer ts.Close()
 
-		resp, err := client.Get(endpoint.String()+handlerURL, http.Header{})
-		require.Nil(t, err)
-		require.Equal(t, 500, resp.StatusCode)
-		require.Equal(t, "application/problem+json", resp.Header["Content-Type"][0])
+	resp, err := http.Get(ts.URL + handlerURL)
+	require.Nil(t, err)
 
-		bs, err := ioutil.ReadAll(resp.Body)
-		defer resp.Body.Close()
-		require.Nil(t, err)
+	require.Equal(t, 500, resp.StatusCode)
+	require.Equal(t, "application/problem+json", resp.Header["Content-Type"][0])
 
-		var msg map[string]interface{}
-		err = json.Unmarshal(bs, &msg)
-		require.Nil(t, err)
-		require.Equal(t, "panic: "+panicMsg, msg["title"])
-	}
+	bs, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	require.Nil(t, err)
+
+	var msg map[string]interface{}
+	err = json.Unmarshal(bs, &msg)
+	require.Nil(t, err)
+	require.Equal(t, "panic: "+panicMsg, msg["title"])
 }
