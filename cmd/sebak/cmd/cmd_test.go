e8be67f77063c79a1b38dd41699c5eb706cb98f5
Spike^ekipS <spikeekips@gmail.com>
2018-10-11 14:07:29 +0900

    more test cases added

--- cmd/sebak/cmd/cmd_test.go
+++ cmd/sebak/cmd/cmd_test.go
@@ -30,7 +30,7 @@ func TestParseFlagSelfValidators(t *testing.T) {
 	require.Equal(t, validator.Endpoint().Port(), parsedValidator.Endpoint().Port())
 }
 
-func TestAddingSelfValidators(t *testing.T) {
+func TestAddingSelfValidatorsWithoutSelf(t *testing.T) {
 	flagNetworkID = "sebak-test-network"
 	flagValidators = "https://localhost:12346?address=GDPQ2LBYP3RL3O675H2N5IEYM6PRJNUA5QFMKXIHGTKEB5KS5T3KHFA2"
 	flagKPSecretSeed = "SCN4NSV5SVHIZWUDJFT4Z5FFVHO3TFRTOIBQLHMNPAZJ37K5A2YFSCBM"
@@ -61,6 +61,57 @@ func TestAddingSelfValidators(t *testing.T) {
 		}
 		require.True(t, found)
 	}
+}
+
+func TestAddingSelfValidatorsWithSelf(t *testing.T) {
+	targetValidators := "https://localhost:12346?address=GDPQ2LBYP3RL3O675H2N5IEYM6PRJNUA5QFMKXIHGTKEB5KS5T3KHFA2"
+
+	flagNetworkID = "sebak-test-network"
+	flagValidators = "self " + targetValidators
+	flagKPSecretSeed = "SCN4NSV5SVHIZWUDJFT4Z5FFVHO3TFRTOIBQLHMNPAZJ37K5A2YFSCBM"
+	flagBindURL = "http://0.0.0.0:12345"
+
+	parseFlagsNode()
+
+	require.NotNil(t, localNode)
+	require.Equal(t, 2, len(localNode.GetValidators()))
+
+	{ // check validator added
+		var found bool
+		v, _ := node.NewValidatorFromURI(targetValidators)
+		for _, validator := range localNode.GetValidators() {
+			if v.Address() == validator.Address() {
+				found = true
+			}
+		}
+		require.True(t, found)
+	}
+
+	{ // check LocalNode added
+		var found bool
+		for _, validator := range localNode.GetValidators() {
+			if localNode.Address() == validator.Address() {
+				found = true
+			}
+		}
+		require.True(t, found)
+	}
+}
+
+func TestAddingSelfValidatorsWithOnlySelf(t *testing.T) {
+	flagNetworkID = "sebak-test-network"
+	flagValidators = "self"
+	flagKPSecretSeed = "SCN4NSV5SVHIZWUDJFT4Z5FFVHO3TFRTOIBQLHMNPAZJ37K5A2YFSCBM"
+	flagBindURL = "http://0.0.0.0:12345"
+
+	parseFlagsNode()
+
+	require.NotNil(t, localNode)
+	require.Equal(t, 1, len(localNode.GetValidators()))
+
+	validator, ok := localNode.GetValidators()[localNode.Address()]
+	require.True(t, ok)
+	require.Equal(t, localNode.Address(), validator.Address())
 }
 
 func TestAddingSelfWithoutValidators(t *testing.T) {
