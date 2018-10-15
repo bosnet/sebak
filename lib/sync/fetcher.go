0a125296f0ca3da5b5adefdfaf5f44dd1e362584
Charles <charleslee522@gmail.com>
2018-10-15 14:58:00 +0900

    Log after checking nil

--- lib/sync/fetcher.go
+++ lib/sync/fetcher.go
@@ -99,10 +99,10 @@ func (f *BlockFetcher) fetch(ctx context.Context, si *SyncInfo) error {
 	f.logger.Debug("Fetch start", "height", height)
 
 	n := f.pickRandomNode(nodeAddrs)
-	f.logger.Info(fmt.Sprintf("fetching items from node: %v", n), "fetching_node", n, "height", height)
 	if n == nil {
 		return errors.New("Fetch: node not found")
 	}
+	f.logger.Info(fmt.Sprintf("fetching items from node: %v", n), "fetching_node", n, "height", height)
 
 	apiURL := apiClientURL(n, height)
 	f.logger.Debug("apiClient", "url", apiURL.String())
