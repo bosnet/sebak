5dc69ab9a51f7ec444136bf14ba71488d0443e7b
Spike^ekipS <spikeekips@gmail.com>
2018-10-18 13:06:45 +0900

    revert PingTest

--- tests/client/ping_test.go
+++ tests/client/ping_test.go
@@ -1,17 +1,21 @@
 package client
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
 	"boscoin.io/sebak/lib/client"
-	"github.com/stretchr/testify/require"
 )
 
 func TestPing(t *testing.T) {
 	c := client.NewClient("https://127.0.0.1:2830")
 	headers := http.Header{}
 	headers.Set("Content-Type", "application/json")
-	_, e := c.LoadAccount("GDIRF4UWPACXPPI4GW7CMTACTCNDIKJEHZK44RITZB4TD3YUM6CCVNGJ")
-	require.NotNil(t, e)
+
+	a, e := c.LoadAccount("GDIRF4UWPACXPPI4GW7CMTACTCNDIKJEHZK44RITZB4TD3YUM6CCVNGJ")
+	if e != nil {
+		fmt.Println(e)
+	}
+	fmt.Println(a)
 }
