6c7bf0c432fc686ebe25d4eb95023d2213befbbc
Charles <charleslee522@gmail.com>
2018-10-26 14:00:56 +0900

    Fix typo commitable -> committable

--- lib/error/errors.go
+++ lib/error/errors.go
@@ -79,5 +79,5 @@ var (
 	ErrorTooManyRequests                           = NewError(172, "too many requests; reached limit")
 	ErrorHTTPServerError                           = NewError(173, "Internal Server Error")
 	ErrorBlockTransactionHistoryDoesNotExists      = NewError(174, "transaction history does not exists in block")
-	ErrorNotCommitableCore                         = NewError(175, "not CommittableBackend")
+	ErrorNotCommittableCore                        = NewError(175, "not CommittableCore")
 )
