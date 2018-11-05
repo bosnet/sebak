d6c15b83271549221cf90ffa6653f0e2eef1df83
Spike^ekipS <spikeekips@gmail.com>
2018-11-05 15:26:34 +0900

    Fixed to return the transaction message in `GetBlocksHandler()` (#643)
    https://github.com/bosnet/sebak/issues/640

--- lib/node/runner/api_block_test.go
+++ lib/node/runner/api_block_test.go
@@ -59,6 +59,7 @@ func (p *HelperTestGetBlocksHandler) createBlock() block.Block {
 	for _, tx := range txs {
 		btx := block.NewBlockTransactionFromTransaction(bk.Hash, bk.Height, bk.Confirmed, tx)
 		btx.MustSave(p.st)
+		block.SaveTransactionPool(p.st, tx)
 	}
 
 	return bk
