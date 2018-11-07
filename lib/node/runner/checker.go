b010f0bccdb95268ef8013e496f0c496cf6f8c16
Mathias LANG <geod24@gmail.com>
2018-11-07 18:30:25 +0900

    Revert "Implement delayed operations"
    This reverts commit a3f2684316058f3d75386bea9375784493378ef6.

--- lib/node/runner/checker.go
+++ lib/node/runner/checker.go
@@ -5,6 +5,8 @@ import (
 	"bytes"
 	"io"
 
+	logging "github.com/inconshreveable/log15"
+
 	"boscoin.io/sebak/lib/ballot"
 	"boscoin.io/sebak/lib/block"
 	"boscoin.io/sebak/lib/common"
@@ -15,7 +17,6 @@ import (
 	"boscoin.io/sebak/lib/transaction"
 	"boscoin.io/sebak/lib/transaction/operation"
 	"boscoin.io/sebak/lib/voting"
-	logging "github.com/inconshreveable/log15"
 )
 
 type CheckerStopCloseConsensus struct {
@@ -624,7 +625,6 @@ func saveBlock(checker *BallotChecker) error {
 	theBlock, err = finishBallotWithProposedTxs(
 		bs,
 		checker.Ballot,
-		checker.NodeRunner.TransactionPool.Pending,
 		proposedTransactions,
 		checker.Log,
 		checker.NodeRunner.Log(),
