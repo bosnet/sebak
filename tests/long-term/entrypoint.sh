be035398f6d7a78a623896dae74fffe979dd686d
Charles <charleslee522@gmail.com>
2018-11-12 18:14:43 +0900

    Make long-term tests enrich
    add create-account transaction in integration-tests
    add payment txs
    add syncing all nodes
    add tx after syncing
    add checking common accounts

--- tests/long-term/entrypoint.sh
+++ tests/long-term/entrypoint.sh
@@ -2,9 +2,25 @@
 
 set -xe
 
-if [ $# -ne 1 ]; then
-    echo 1>&2 "Error: Expected 1 argument (test directory), $# provided..."
+
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+    echo 1>&2 "Error: Expected 1(test directory) or 2 argument (block_time.sh SECONDS), $# provided..."
     exit 1
 fi
 
-./$1
+if [ $# -eq 1 ]; then
+  if [ -d "${1}" ]
+  then
+    echo "run test"
+    TEST_DIR=${1}
+    ./default_runner.sh ${TEST_DIR}
+  else
+    ./${1}
+  fi
+fi
+
+if [ $# -eq 2 ]; then
+    echo "check block time"
+   ./${1} ${2}
+fi
+
