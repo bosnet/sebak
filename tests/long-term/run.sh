e8503284b0bdc644a59a578afa610e53fdf9f96d
Charles <charleslee522@gmail.com>
2018-11-13 09:23:50 +0900

    add loglevel=debug temporarily for debuging

--- tests/long-term/run.sh
+++ tests/long-term/run.sh
@@ -37,13 +37,13 @@ fi
 # because the reports are written on program's exit, which also means container's shutdown
 # Also SUPER IMPORTANT: the `-test` args need to be before any other args, or they are simply ignored...
 export NODE1=$(docker run -d --network host --env-file=${ROOT_DIR}/docker/node1.env \
-                        ${NODE_IMAGE} node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON})
+                        ${NODE_IMAGE} node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON} --log-level=debug)
 export NODE2=$(docker run -d --network host --env-file=${ROOT_DIR}/docker/node2.env \
-                        ${NODE_IMAGE} node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON})
+                        ${NODE_IMAGE} node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON} --log-level=debug)
 export NODE3=$(docker run -d --network host --env-file=${ROOT_DIR}/docker/node3.env \
-                        ${NODE_IMAGE} node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON})
+                        ${NODE_IMAGE} node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON} --log-level=debug)
 export NODE4=$(docker run -d --network host --env-file=${ROOT_DIR}/docker/node4.env \
-                        ${NODE_IMAGE} node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON})
+                        ${NODE_IMAGE} node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON} --log-level=debug)
 
 CONTAINERS="${CONTAINERS} ${NODE1} ${NODE2} ${NODE3} ${NODE4}"
 
@@ -64,7 +64,7 @@ docker stop ${NODE4}
 docker rm -f ${NODE4}
 
 export NODE4_2=$(docker run -d --network host --env-file=${ROOT_DIR}/docker/node4.env \
-                        ${NODE_IMAGE} -test.coverprofile=coverage.txt node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON})
+                        ${NODE_IMAGE} -test.coverprofile=coverage.txt node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON} --log-level=debug)
 
 CONTAINERS="${CONTAINERS} ${NODE4_2}"
 
@@ -76,7 +76,7 @@ docker stop ${NODE3}
 docker rm -f ${NODE3}
 
 export NODE3_2=$(docker run -d --network host --env-file=${ROOT_DIR}/docker/node3.env \
-                        ${NODE_IMAGE} -test.coverprofile=coverage.txt node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON})
+                        ${NODE_IMAGE} -test.coverprofile=coverage.txt node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON} --log-level=debug)
 
 CONTAINERS="${CONTAINERS} ${NODE3_2}"
 
@@ -88,7 +88,7 @@ docker stop ${NODE2}
 docker rm -f ${NODE2}
 
 export NODE2_2=$(docker run -d --network host --env-file=${ROOT_DIR}/docker/node2.env \
-                        ${NODE_IMAGE} -test.coverprofile=coverage.txt node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON})
+                        ${NODE_IMAGE} -test.coverprofile=coverage.txt node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON} --log-level=debug)
 
 CONTAINERS="${CONTAINERS} ${NODE2_2}"
 
@@ -100,7 +100,7 @@ docker stop ${NODE1}
 docker rm -f ${NODE1}
 
 export NODE1_2=$(docker run -d --network host --env-file=${ROOT_DIR}/docker/node1.env \
-                        ${NODE_IMAGE} -test.coverprofile=coverage.txt node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON})
+                        ${NODE_IMAGE} -test.coverprofile=coverage.txt node --genesis=${SEBAK_GENESIS},${SEBAK_COMMON} --log-level=debug)
 
 CONTAINERS="${CONTAINERS} ${NODE1_2}"
 
