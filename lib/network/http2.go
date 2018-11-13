fc1a301d8fb2be65383fd9a87c67bd6474c6f89b
anarcher <anarcher@gmail.com>
2018-11-13 14:53:01 +0900

    Change metric_path to `/metrics`

--- lib/network/http2.go
+++ lib/network/http2.go
@@ -22,7 +22,7 @@ type Handlers map[string]func(http.ResponseWriter, *http.Request)
 const (
 	RouterNameNode   = "node"
 	RouterNameAPI    = "api"
-	RouterNameMetric = "metric"
+	RouterNameMetric = "metrics"
 	RouterNameDebug  = "debug"
 )
 
