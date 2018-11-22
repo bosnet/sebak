0ad7d179ed27e92c5094def7a004e0c017b0183e
Charles <charleslee522@gmail.com>
2018-11-22 13:47:36 +0900

    Send expired ballot to itself

--- lib/node/runner/isaac_state_manager.go
+++ lib/node/runner/isaac_state_manager.go
@@ -267,7 +267,7 @@ func (sm *ISAACStateManager) broadcastExpiredBallot(round uint64, state ballot.S
 	newExpiredBallot.Sign(sm.nr.localNode.Keypair(), sm.nr.Conf.NetworkID)
 
 	sm.nr.Log().Debug("broadcast", "ballot", *newExpiredBallot)
-	sm.nr.ConnectionManager().Broadcast(*newExpiredBallot)
+	sm.nr.broadcastBallot(*newExpiredBallot)
 }
 
 func (sm *ISAACStateManager) resetTimer(timer *time.Timer, state ballot.State) {
