0d5d1b2abc8a7e5e9b62dbd7e539b5b5dd0da0fa
Charles <charleslee522@gmail.com>
2018-12-04 21:37:15 +0900

    Remove calculateAverageBlockTime
    Because it is reduntant. We need just sinceGenesis
    Add documents for calculateBlockTimeBuffer

--- lib/node/runner/block_time_buffer_test.go
+++ lib/node/runner/block_time_buffer_test.go
@@ -7,19 +7,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCalculateAverageBlockTime(t *testing.T) {
-	now := time.Now()
-	lastDay := now.AddDate(0, 0, -1)
-
-	nBlocksInOneDay := 720 * 24
-	height := uint64(nBlocksInOneDay)
-
-	blockTime := calculateAverageBlockTime(lastDay, height)
-	require.True(t, blockTime > 4900*time.Millisecond)
-	require.True(t, blockTime < 5100*time.Millisecond)
-
-}
-
 func TestCalculateBlockTimeBuffer(t *testing.T) {
 	require.Equal(t, 1*time.Second, calculateBlockTimeBuffer(
 		1,
@@ -56,7 +43,7 @@ func TestCalculateBlockTimeBuffer(t *testing.T) {
 
 // We can check that after one year,
 // if it took 6 sec to confirm one block,
-// the next block should be confirmed about 4 seconds later.
+// the next block should be confirmed 4 seconds later.
 func TestLongTermBlockTimeBuffer(t *testing.T) {
 	oneYearHeight := uint64(365 * 24 * 60 * 60 / 5)
 	oneYearSeconds := time.Duration(oneYearHeight) * 5 * time.Second
@@ -64,17 +51,11 @@ func TestLongTermBlockTimeBuffer(t *testing.T) {
 	oneYearHeight++
 	oneYearSeconds += 6 * time.Second
 
-	averageOneHeightAfter := oneYearSeconds / time.Duration(oneYearHeight)
-
-	require.Equal(t, 4003462242*time.Nanosecond, calculateBlockTimeBuffer(
+	require.Equal(t, 4*time.Second, calculateBlockTimeBuffer(
 		oneYearHeight,
 		5*time.Second,
-		averageOneHeightAfter,
+		oneYearSeconds,
 		0*time.Second,
 		1*time.Second,
 	))
-
-	oneYearSeconds += 4003462242 * time.Nanosecond
-	averageTwoHeightAfter := oneYearSeconds / time.Duration(oneYearHeight+1)
-	require.Equal(t, 5*time.Second, averageTwoHeightAfter)
 }
