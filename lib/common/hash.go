b3ccf8131bf5f1d83d9687995ab42abf0ef63938
Geod24 <pro.mathias.lang@gmail.com>
2018-12-04 12:02:38 +0100

    Document lib/common/hash.go and add Encoder type

--- lib/common/hash.go
+++ lib/common/hash.go
@@ -1,3 +1,8 @@
+//
+// This package defines hash-related functions used in Sebak
+// The primary hash function used in Sebak is a double sha256,
+// like Bitcoin.
+//
 package common
 
 import (
@@ -7,12 +12,26 @@ import (
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
+// 32 bytes / 256 bits hash type
+type Hash = ethcommon.Hash
+
+// Set the content of the hash to be the provided binary data
+var BytesToHash = ethcommon.BytesToHash
+
+// Generate a double SHA-256 hash out of the supplied binary data
 func MakeHash(b []byte) []byte {
 	first := sha256.Sum256(b)
 	second := sha256.Sum256(first[:])
 	return second[:]
 }
 
+// Interface recognized by RLP encoder
+type Encoder = rlp.Encoder
+
+// Generate a double SHA-256 hash from the interface's RLP encoding
+//
+// Types wishing to implement custom encoding should implement
+// the `Encoder.EncodeRLP` interface
 func MakeObjectHash(i interface{}) (b []byte, err error) {
 	var e []byte
 	if e, err = rlp.EncodeToBytes(i); err != nil {
@@ -24,6 +43,7 @@ func MakeObjectHash(i interface{}) (b []byte, err error) {
 	return
 }
 
+// Pedestrian version of `MakeObjectHash`
 func MustMakeObjectHash(i interface{}) []byte {
 	if b, err := MakeObjectHash(i); err != nil {
 		panic(err)
@@ -32,10 +52,8 @@ func MustMakeObjectHash(i interface{}) []byte {
 	}
 }
 
+// Returns the hash of the object, base58 encoded
 func MustMakeObjectHashString(i interface{}) string {
 	b := MustMakeObjectHash(i)
 	return base58.Encode(b)
 }
-
-type Hash = ethcommon.Hash
-var BytesToHash = ethcommon.BytesToHash
