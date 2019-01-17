30c984c10695a39bf5ffb6811df0dfd6213d580d
Geod24 <pro.mathias.lang@gmail.com>
2019-01-17 20:22:15 +0900

    Improve and document observer code

--- lib/common/observer/observer.go
+++ lib/common/observer/observer.go
@@ -9,31 +9,55 @@ var SyncBlockWaitObserver = observable.New()
 
 var ResourceObserver = observable.New()
 
-type ResourceTy = string
+// The type of ressource that the event concerns
+type ResourceType = string
 
-type KeyTy = string
+// The type of key that the event applies to (depends on the ressource)
+type KeyType = string
 
 const (
-	Tx     ResourceTy = "tx"
-	TxPool            = "txpool"
-	Acc               = "acc"
+	// An event relative to transactions
+	Tx ResourceType = "tx"
+	// An event relative to transactions in the transaction pool
+	TxPool = "txpool"
+	// An event relative to accounts (creation, update)
+	Acc = "acc"
 )
 
 const (
-	All     KeyTy = "*"
-	Source        = "source"
-	Target        = "target"
-	TxHash        = "txhash"
-	Address       = "address"
+	// All events related to the `ResourceType`
+	All KeyType = "*"
+	// Tx/TxPool only: Transactions with a specified source
+	Source = "source"
+	// Tx/TxPool only: Transactions with a specified target
+	Target = "target"
+	// Tx/TxPool only: Transactions with a specific hash
+	TxHash = "txhash"
+	// Acc only: Only stream updates related to the specified address(es)
+	Address = "address"
 )
 
+// A Condition can be sent as the body when calling subscribe
 type Condition struct {
-	Resource ResourceTy `json:"resource"`
-	Key      KeyTy      `json:"key"`
-	Value    string     `json:"value"`
+	// Affected ressource
+	Resource ResourceType `json:"resource"`
+	// Filter to use (can be `All)
+	Key KeyType `json:"key"`
+	// If `Key != All`, value of the filter
+	Value string `json:"value"`
 }
 
-func NewCondition(resource ResourceTy, key KeyTy, v ...string) Condition {
+//
+// Instantiate a new `Condition` object
+//
+// Params:
+//     ressource = The requested `RessourceType` to stream
+//     key       = The key to stream
+//     v         = An optional value for key. Only the first value will be used.
+//
+// Returns: A `Condition` object, that can be triggered or passed to `/subscribe`
+//
+func NewCondition(resource ResourceType, key KeyType, v ...string) Condition {
 	value := ""
 	if len(v) > 0 {
 		value = v[0]
@@ -45,6 +69,7 @@ func NewCondition(resource ResourceTy, key KeyTy, v ...string) Condition {
 	}
 }
 
+// Returns: the serialized (as a string) name of this event)
 func (c Condition) Event() string {
 	toStr := c.Resource + "-"
 	if c.Key == All {
@@ -56,6 +81,7 @@ func (c Condition) Event() string {
 	return toStr
 }
 
+// An array of Condition
 type Conditions []Condition
 
 func (cs Conditions) Event() string {
